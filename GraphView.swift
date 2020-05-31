b4d519217bd0b4bb96fa4f598ad87915744bcf60
ldhough <ldhough2000@gmail.com>
2020-05-31 00:20:38 -0400

    Changes to make bar graph more dynamic

--- GraphView.swift
+++ GraphView.swift
@@ -7,10 +7,18 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct GraphView: View {
     
-    let base:CGFloat = 400
+    let base:CGFloat = 400 // Bottom edge of graph
+    let start:CGFloat
+    let frameHeight:CGFloat// = Constants.SCREEN_WIDTH*0.75 // Only square frames being used for now
+    
+    init() {
+        self.frameHeight = Constants.SCREEN_WIDTH*0.75
+        self.start = (Constants.SCREEN_WIDTH-self.frameHeight)/2
+    }
     
     let colors:[Color] = [Color.blue, Color.red, Color.purple, Color.yellow, Color.green, Color.orange]
     
@@ -29,21 +37,35 @@ struct GraphView: View {
     
     func labelsText() -> some View {
         ForEach(0 ..< bars.count) { i in
-            self.rotatedText(i: i).position(x: (100 + 1.5*(CGFloat(i)*self.width())), y: self.base+15)
+            self.rotatedText(i: i).position(x: (self.start + 1.5*(CGFloat(i)*self.width())), y: self.base+25)
         }
     }
     
     func makeBars() -> some View {
         ForEach(0 ..< bars.count) { i in
             Path { path in
-                path.move(to: CGPoint(x: (100 + 1.5*(CGFloat(i)*self.width())), y: self.base))
-                path.addLine(to: CGPoint(x: 100 + 1.5*(CGFloat(i)*self.width()), y: (self.base-CGFloat(self.bars[i]*10))))
+                path.move(to: CGPoint(x: (self.start + 1.5*(CGFloat(i)*self.width())), y: self.base))
+                path.addLine(to: CGPoint(x: self.start + 1.5*(CGFloat(i)*self.width()), y: (self.base-CGFloat(self.bars[i]*10))))
             }.stroke(self.colors[i], lineWidth: self.width())
+        }
+    }
+    
+    func makeEnclosure() -> some View {
+        Group {
+            Path { path in // Horizontal bounding line
+                path.move(to: CGPoint(x: self.start-20, y: self.base+3))
+                path.addLine(to: CGPoint(x: self.frameHeight+self.start, y: self.base+3))
+            }.stroke(Color.black, lineWidth: 3)
+            Path { path in // Vertical bounding line
+                path.move(to: CGPoint(x: self.start-20, y: self.base+3))
+                path.addLine(to: CGPoint(x: self.start-20, y: (self.base-frameHeight)))
+            }.stroke(Color.black, lineWidth: 3)
         }
     }
     
     var body: some View {
         ZStack {
+            self.makeEnclosure()
             self.makeBars()
             self.labelsText()
         }
